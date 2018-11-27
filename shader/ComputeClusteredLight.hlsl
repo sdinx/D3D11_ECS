70e7b9de805779239c88e7bba9951abfa166a2db
Sakai <inx.sakai@gmail.com>
2018-11-27 09:47:58 +0900

    merge.

--- shader/ComputeClusteredLight.hlsl
+++ shader/ComputeClusteredLight.hlsl
@@ -1,9 +1,11 @@
 #include "header/Structs.hlsli"
 
-const uint MAX_LIGHT_COUNT = 1000;
-const uint MAX_X_PLANE = 16;
-const uint MAX_Y_PLANE = 16;
-const uint MAX_Z_PLANE = 16;
+const int MAX_LIGHT_COUNT = 1000;
+const int MAX_X_PLANE = 16;
+const int MAX_Y_PLANE = 16;
+const int MAX_Z_PLANE = 16;
+const int NUM_SLICE_COUNT = 16;
+const int NUM_TILE_GROUP_PIXELS = 16;
 
 [numthreads( MAX_LIGHT_COUNT, 1, 1 )]
 void main( uint3 DTid : SV_DispatchThreadID )
@@ -30,7 +32,6 @@ void main( uint3 DTid : SV_DispatchThreadID )
 
 }
 
-const uint NUM_SLICE_COUNT = 16;
 
 [numthreads( MAX_LIGHT_COUNT, 1, 1 )]
 void LightCulling( uint3 DTid : SV_DispatchThreadID )
@@ -41,6 +42,23 @@ void LightCulling( uint3 DTid : SV_DispatchThreadID )
     float h0 = ( 2 * near * tanFov ) / NUM_SLICE_COUNT;
 
     uint k = log( -z / near ) / log( 1 + ( 2 * tanFov / NUM_SLICE_COUNT ) );
+
+}
+
+
+void ComputeCluster()
+{
+    const uint a = NUM_SLICE_COUNT;
+    uint sliceCount = NUM_SLICE_COUNT;
+    float z = 0;
+    int k = 0;
+
+	// 指数関数的にフラスタムを分割
+    for ( uint sliceIndex = 1; sliceIndex <= sliceCount; sliceIndex++ )
+    {
+        z = lerp( 0.0f, 1.0f, log( sliceIndex / sliceCount * ( a - 1 ) + 1 ) / log( a ) );
+
+    } // end for
 
 }
 
